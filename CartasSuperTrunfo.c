6aa5adf3564cd8a29ec09ab8eadfa1f8df66b587
ZiniDiegoR <diego.diniz23@outlook.com>
2026-03-31 18:53:34 -0300

    cartaSuperTrunfo.c
    edição do novo desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,93 +1,93 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+int main() {
+    char estado1, estado2;
+    char codigo1[4], codigo2[4];
+    char cidade1[100], cidade2[100];
+    unsigned long int populacao1, populacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontosTuristicos1, pontosTuristicos2;
 
-// CARTA 1
-  char estado1;
-  char codigo1[10];
-  char saopaulo1[50];
-  int populacao1;
-  float area1;
-  float pib1;
-  int pontost1;
+    float densidade1, densidade2;
+    float pibPerCapita1, pibPerCapita2;
 
+    printf("Cadastro da Carta 1:\n");
+    printf("Estado: ");
+    scanf(" %c", &estado1);
 
-  // CARTA 2
-  char estado2;
-  char codigo2[10];
-  char riodejaneiro2[50];
-  int populacao2;
-  float area2;
-  float pib2;
-  int pontost2;
+    printf("Codigo da carta: ");
+    scanf("%3s", codigo1);
 
-  int opcao;
+    printf("Nome da cidade: ");
+    scanf(" ");
+    fgets(cidade1, sizeof(cidade1), stdin);
+    cidade1[strcspn(cidade1, "\n")] = '\0';
 
-  // CADASTRO CARTA 1
+    printf("Populacao: ");
+    scanf("%lu", &populacao1);
 
-  printf("CADASTRO CARTA 1\n");
-  printf("estado1:");
-  scanf("%c", &estado1);
+    printf("Area (km²): ");
+    scanf("%f", &area1);
 
-  printf("codigo1:");
-  scanf("%s", codigo1);
+    printf("PIB (em bilhoes de reais): ");
+    scanf("%f", &pib1);
 
-  printf("saopaulo1:");
-  scanf("%s", saopaulo1);
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontosTuristicos1);
 
-  printf("populacao1:");
-  scanf("%d", &populacao1);
+    printf("\nCadastro da Carta 2:\n");
+    printf("Estado: ");
+    scanf(" %c", &estado2);
 
-  printf("area1:");
-  scanf("%f", &area1);
+    printf("Codigo da carta: ");
+    scanf("%3s", codigo2);
 
-  printf("pib1:");
-  scanf("%f", &pib1);
+    printf("Nome da cidade: ");
+    scanf(" ");
+    fgets(cidade2, sizeof(cidade2), stdin);
+    cidade2[strcspn(cidade2, "\n")] = '\0';
 
-  printf("pontost1:");
-  scanf("%d", &pontost1);
+    printf("Populacao: ");
+    scanf("%lu", &populacao2);
 
-  // CADASTRO CARTA 2
+    printf("Area (km²): ");
+    scanf("%f", &area2);
 
-  printf("CADASTRO CARTA 2\n");
-  printf("estado2:");
-  scanf(" %c", &estado2); // Note the space before %c to consume
+    printf("PIB (em bilhoes de reais): ");
+    scanf("%f", &pib2);
 
-  printf("codigo2:");
-  scanf("%s", codigo2);
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &pontosTuristicos2);
 
-  printf("riodejaneiro2:");
-  scanf("%s", riodejaneiro2);
+    densidade1 = populacao1 / area1;
+    densidade2 = populacao2 / area2;
 
-  printf("populacao2:");
-  scanf("%d", &populacao2);
+    pibPerCapita1 = (pib1 * 1000000000.0f) / populacao1;
+    pibPerCapita2 = (pib2 * 1000000000.0f) / populacao2;
 
-  printf("area2:");
-  scanf("%f", &area2);
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("Populacao: %lu\n", populacao1);
+    printf("Area: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
 
-  printf("pib2:");
-  scanf("%f", &pib2);
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("Populacao: %lu\n", populacao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Numero de Pontos Turisticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
 
-  printf("pontost2:");
-  scanf("%d", &pontost2);
-
-  return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
